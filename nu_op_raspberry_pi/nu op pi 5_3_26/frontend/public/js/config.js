84f940c6e696189a3f6b6e3e4719ad4d19f7dbc7
arnehermans08 <arnehermans@leerling.wico.be>
2026-03-05 11:33:54 +0100

    style change

--- nu_op_raspberry_pi/nu op pi 5_3_26/frontend/public/js/config.js
+++ nu_op_raspberry_pi/nu op pi 5_3_26/frontend/public/js/config.js
@@ -3,13 +3,13 @@ let trackConfig = {
     segmentCount: 4,
     cartCount: 1,
     segmentNames: ["appel"],
-    colors: ['#379837', '#ffb339', '#ff2c2c'],  // index 0=green, 1=orange, 2=red
+    colors: ['#2E7D32', '#ffb339', '#C62828'],  // index 0=green, 1=orange, 2=red
     
     sensorThresholds: {
         temp: { 
             1: 0,    // waarde >=0  → kleur op index 0 (green)
-            2: 60,   // waarde >=40 → kleur op index 1 (orange)
-            3: 80    // waarde >=70 → kleur op index 2 (red)
+            2: 60,   // waarde >=60 → kleur op index 1 (orange)
+            3: 80    // waarde >=80 → kleur op index 2 (red)
         }
     }
 };
